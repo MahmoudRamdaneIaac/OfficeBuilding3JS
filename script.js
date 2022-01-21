7a63a145398cb105701aa4349d55a7473dbba052
Mahmoud Ramdane <ramdane.mahmoud@students.iaac.net>
2022-01-21 22:14:00 +0100

    21.1

--- script.js
+++ script.js
@@ -33,19 +33,22 @@ function init() {
     document.body.appendChild( renderer.domElement )
   
     
+const controls = new OrbitControls( camera, renderer.domElement )
 
 
 
-    const controls = new OrbitControls( camera, renderer.domElement )
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff )
-    directionalLight.position.set( 0, 0, 30 )
-    directionalLight.castShadow = true
-    directionalLight.intensity = 2
 
-    scene.add( directionalLight )
 
-    raycaster = new THREE.Raycaster()
+//LIGHT
+    //const directionalLight = new THREE.DirectionalLight( 0xffffff )
+    //directionalLight.position.set( 0, 0, 30 )
+    //directionalLight.castShadow = true
+    //directionalLight.intensity = 2
+
+    //scene.add( directionalLight )
+
+    //raycaster = new THREE.Raycaster()
 
     //add 3DM file here
     
@@ -61,6 +64,82 @@ function init() {
     } )
 
 }
+
+// LIGHTS
+
+const hemiLight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 0.22 );
+hemiLight.color.setHSL( 0.6, 1, 0.6 );
+hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+hemiLight.position.set( 0, 50, 0 );
+scene.add( hemiLight );
+
+//const hemiLightHelper = new THREE.HemisphereLightHelper( hemiLight, 10 );
+//scene.add( hemiLightHelper );
+
+//
+
+const dirLight = new THREE.DirectionalLight( 0xffffff,  );
+dirLight.color.setHSL( 0.1, 1, 0.95 );
+dirLight.position.set( - 1, 1.75, 1 );
+dirLight.position.multiplyScalar( 60);
+scene.add( dirLight );
+
+dirLight.castShadow = true;
+
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+
+const d = 50;
+
+dirLight.shadow.camera.left = - d;
+dirLight.shadow.camera.right = d;
+dirLight.shadow.camera.top = d;
+dirLight.shadow.camera.bottom = - d;
+
+dirLight.shadow.camera.far = 3500;
+dirLight.shadow.bias = - 0.0001;
+
+//const dirLightHelper = new THREE.DirectionalLightHelper( dirLight, 10 );
+//scene.add( dirLightHelper );
+
+// GROUND
+
+const groundGeo = new THREE.PlaneGeometry( 10000, 10000 );
+const groundMat = new THREE.MeshLambertMaterial( { color: 0xffffff } );
+groundMat.color.setHSL( 0.3, 1, 0.75 );
+
+const ground = new THREE.Mesh( groundGeo, groundMat );
+ground.position.y =   0;
+ground.rotation.x = 0;
+ground.receiveShadow = true;
+scene.add( ground );
+
+// SKYDOME
+
+const vertexShader = document.getElementById( 'vertexShader' ).textContent;
+const fragmentShader = document.getElementById( 'fragmentShader' ).textContent;
+const uniforms = {
+    "topColor": { value: new THREE.Color( 0x0077ff ) },
+    "bottomColor": { value: new THREE.Color( 0xffffff ) },
+    "offset": { value: 33 },
+    "exponent": { value: 0.6 }
+};
+uniforms[ "topColor" ].value.copy( hemiLight.color );
+
+scene.fog.color.copy( uniforms[ "bottomColor" ].value );
+
+const skyGeo = new THREE.SphereGeometry( 4000, 32, 15 );
+const skyMat = new THREE.ShaderMaterial( {
+    uniforms: uniforms,
+    vertexShader: vertexShader,
+    fragmentShader: fragmentShader,
+    side: THREE.BackSide
+} );
+
+const sky = new THREE.Mesh( skyGeo, skyMat );
+scene.add( sky );
+
+
 
 function onClick( event ) {
 
